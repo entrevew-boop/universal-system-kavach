0604fef48c98c7461859e6b31c5e035a4a9d2c08
entrevew-boop <entrevew@gmail.com>
2026-08-17 17:56:26 +0530

    Update sserver.py

--- sserver.py
+++ sserver.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
 def home():
